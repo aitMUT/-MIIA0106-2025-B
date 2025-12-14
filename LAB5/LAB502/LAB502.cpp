4c0d0c3b3e234c5bcf74296bc5a4762f017e4948
aitMut <6812100020@mut.ac.th>
2025-12-14 17:22:41 +0700

    Lab5

---
+++ LAB5/LAB502/LAB502.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int maxmaxOfThree(int a, int b, int c)
+{
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+		return c;
+}
+
+int minvalueOfThree(int a, int b, int c)
+{
+    if (a <= b && a <= c)
+        return a;
+    else if (b <= a && b <= c)
+        return b;
+    else
+        return c;
+}
+
+
+
+int main()
+{
+    int a, b, c;
+    int maxvalue = 1000000;
+
+	cout << "Enter a integers: ";   
+    cin >> a;
+	cout << "Enter b integers: ";   
+	cin >> b;       
+	cout << "Enter c integers: ";   
+    cin >> c;       
+
+	maxvalue = maxmaxOfThree(a, b, c);  
+	cout << "The maximum value is: " << maxvalue << endl;    
+	minvalue = minOfThree(a, b, c); 
+	cout << "The minimum value is: " << minvalue << endl;   
+
+    
+    return 0;
+
+}
