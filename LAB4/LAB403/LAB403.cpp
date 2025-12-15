dda570a760531466dcddb5aa32530ebc28503619
aitMut <6812100020@mut.ac.th>
2025-12-15 22:42:14 +0700

    LAB4 01-07

---
+++ LAB4/LAB403/LAB403.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int sum = 1;
+	for (int i = 1; i <= 100; i++) {
+		sum += i;
+	}
+	cout << "tootal:" << sum << endl;
+	return 0;
+}
---
+++ LAB4/LAB404/LAB404.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int id_digits = 20; // เลขท้าย 2 ตัวของรหัสนักศึกษา
+    cout << "Multiplication Table of " << id_digits << endl;
+    cout << "-----------------------------" << endl;
+    // วนลูปตั้งแต่ 1 ถึง 20
+    for (int i = 1; i <= 20; i++) {
+        cout << id_digits << " x " << i << " = " << (id_digits * i) << endl;
+    }
+    return 0;
+}
