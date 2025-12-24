7181e266bc78c37caf53fc2c8184216c21407866
aitMut <6812100020@mut.ac.th>
2025-12-24 23:55:26 +0700

    LAB6 01-04

---
+++ LAB6/LAB603/LAB603.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+void displayStudentInfo(string name, string id, double score, char grade)
+{
+
+    cout << "Student Name: " << name << "\n";
+    cout << "Student ID  : " << id << "\n";
+    cout << "Score       : " << fixed << setprecision(2) << score << "\n";
+    cout << "Grade       : " << grade << "\n";  
+}
+
+void calculateGrade(double score, char& grade)
+{
+    if (score >= 90) grade = 'A';
+    else if (score >= 80) grade = 'B';
+    else if (score >= 70) grade = 'C';
+    else if (score >= 60) grade = 'D';
+    else grade = 'F';
+}
+int main()
+{
+    const int N = 3; // students
+    const int M = 4; // subjects
+    string name[N], id[N];
+    double score[N][M];
+    double total[N];
+    char grade[N];
+    for (int i = 0; i < N; i++) {
+        cout << " Enter student name: ";
+        getline(cin >> ws, name[i]);
+        cout << " Enter student ID: ";
+        cin >> id[i];
+        total[i] = 0; // Initialize total score for student i
+        for (int j = 0; j < M; j++) {
+            cout << "  Enter score for subject " << (j + 1) << ": ";
+            cin >> score[i][j];
+            total[i] += score[i][j]; // Accumulate total score
+        }
+        calculateGrade(total[i] / 4, grade[i]); // Calculate grade based on total score
+    }
+    for (int i = 0; i < N; i++) {
+        cout << "-------------------------------\n";
+        displayStudentInfo(name[i], id[i], total[i], grade[i]);
+    }
+    return 0;
+}
