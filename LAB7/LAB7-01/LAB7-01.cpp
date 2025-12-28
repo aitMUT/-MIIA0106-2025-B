bd64f3a623753f9c8f1c02bf9b0ea61c35647fff
aitMut <6812100020@mut.ac.th>
2025-12-28 17:05:13 +0700

    LAB07

---
+++ LAB7/LAB7-01/LAB7-01.cpp
@@ -0,0 +1,14 @@
+﻿// TODO: เขียนโปรแกรมแสดงค่า x และ *p
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int* p = &x;
+
+    // TODO
+	cout << "Value of x: " << x << endl;
+	cout << "Value of *p: " << *p << endl;  
+
+    return 0;
+}
---
+++ LAB7/LAB7-02/LAB7-02.cpp
@@ -0,0 +1,17 @@
+﻿// TODO: แสดงค่า score, address และ pointer
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 50;
+    int* ptr = &score;
+
+    // TODO
+	cout << "Value of score is \t" << score << endl;   
+	cout << "Value of ptr is \t" << ptr << endl;
+   
+	cout << "address of score is \t" << &score << endl;
+	cout << "address of ptr is \t" << &ptr << endl;
+
+    return 0;
+}
